fede028e60bc9c78fb59215900d587c9cbf4f4c6
Cait <cait.harrigan@mail.utoronto.ca>
2019-11-05 16:54:10 -0500

    remove NULL dates from html

--- commit-gh-pages-files.sh
+++ commit-gh-pages-files.sh
@@ -1,2 +1,2 @@
 hugo
-cd public && git add --all && git commit -m "Publishing to gh-pages" && cd ..
+cd public && sed -i -e '/1 January, 0001 at 00:00 UTC/d' index.html && git add --all && git commit -m "Publishing to gh-pages" && cd ..
