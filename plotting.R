3e50a18d442c1c891501f1732ebbde3d878b2f72
Cait <cait.harrigan@mail.utoronto.ca>
2019-11-04 22:11:03 -0500

    cleaned up sections 1-3

--- plotting.R
+++ plotting.R
@@ -1,89 +1,130 @@
-#load data prepared from STRING database
-load("~/Downloads/scCCnet.RData")
+# plotting.R
+# Author: Cait Harrigan
+# Date: November 2019
+# Version 1.0
 
+# =============================================================================
+# 1. Set Up
+# =============================================================================
+# With the STRING data that we downloaded and previewed previously, we will 
+# create a network (aka. graph) representing protein interactions, and observe how 
+# certain proteins group together in "communities" with shared similarities
+
+# load an R package that will be useful for our task
 if (!require(igraph)) {
   install.packages("igraph")
   library(igraph)
 }
 
+# load data from STRING database, prepared in getData.R
+load("~/Downloads/scCCnet.rdata")
+preparedData <- scCCnet
+#load("./STRING.RData")
 
-igraphToSize <- function(G){
-  # Find the difference in sizes between the largest and second largest communities
-  # in graph G
+
+# =============================================================================
+# 2. Define functions
+# =============================================================================
+
+# We will define a few functions that let us do certain operations on the data.
+
+getGraphSize <- function(G){
+  # Find the difference in sizes between the largest 
+  # and second largest communities in graph G
   # Parameters:
   #   G: an igraph
   #
   # Value: an integer
   
-  #get cluster sizes
+  # get cluster sizes
   sizes <- sizes(cluster_infomap(G))
-  #order sizes as a vector
+  
+  # order sizes as a vector
   sizesOrdered <- as.vector(sizes)
   
-  #find difference between first and second largest community
-  #in some cases there is only one community
+  # in some cases there is only one community
   if (length(sizesOrdered) == 1){
+    # in this case, only return this community
     return(sizesOrdered[1])
   }
   
+  # find difference between first and second largest community
   return(sizesOrdered[1] - sizesOrdered[2])
 }
 
-igraphToRIgraph <- function(G){
+randGraph <- function(G){
   # Make a random graph of same degree as an input igraph G
   # Parameters:
   #   G: an igraph
   #
   # Value: an undirected igraph with same degree as G
   
-  myDegree <- degree(G)
-  return(sample_degseq(myDegree, method = "vl"))
+  d <- degree(G)
+  return(sample_degseq(d, method = "vl"))
 }
 
-#Create an igraph object from scCCnet
-#Exclude the combined scores
-set.seed(12543)
-myG <- graph_from_edgelist(as.matrix(scCCnet[-3]), directed = F)
+# =============================================================================
+# 3. Plot network
 
-#Plot the scCCnet network according to community membership
-#Modified from the FND-MAT-Graphs_and_networks.R Version 1.0
+# This sections is modified from Boris Steipe's 
+# FND-MAT-Graphs_and_networks.R Version 1.0 
+# https://github.com/hyginn/ABC-units
+# =============================================================================
 
-comms <- cluster_infomap(myG)
-myGxy <- layout_with_graphopt(myG, charge = 0.0017, mass=100, spring.constant = .5)
+# Create an igraph object from our STRING network, by plotting the network 
+# according to community membership of each protein (represented as nodes)
 
-oPar <- par(mar= c(0,4,4,4)) # Turn margins off
-plot(myG,
-     layout = myGxy,
-     rescale = F,
-     xlim = c(min(myGxy[,1]) * 0.99, max(myGxy[,1]) * 1.01),
-     ylim = c(min(myGxy[,2]) * 0.99, max(myGxy[,2]) * 1.01),
-     vertex.color=rainbow(max(membership(comms)+1))[membership(comms)+1],
-     vertex.size = 700 + (90 * degree(myG)),
-     vertex.label = NA,
-     main = "scCCnet Network")
-par(oPar)
+# An edge in our network is defined between a pair of proteins. For all the pairs 
+# in our dataset, we want to create an edge. We select these as columns 1 and 2 of 
+# our prepared dataset. 
+stringNetwork <- graph_from_edgelist(as.matrix(preparedData[c(1,2)]), directed = F)
 
-#Size of a node increases with degree, colour is dictated by community membership.
-#We can see that there are several fairly well connected nodes with high degree.
-#These nodes tend to be members of larger communities - this makes sense biologically,
-#we could expect a protein in a large group of interacting proteins has many interactions.
-#There are two distinctly large and well connected communities.
+# With functions from iGraph package, prepare network for plotting
+communities <- cluster_infomap(stringNetwork)
+networkLayout <- layout_with_graphopt(stringNetwork, charge = 0.002, mass=100, spring.constant = 0.5)
+
+# Plot!
+plot( stringNetwork,
+      layout = networkLayout,
+      rescale = F,
+      xlim = c(min(networkLayout[,1]) * 0.99, max(networkLayout[,1]) * 1.01),
+      ylim = c(min(networkLayout[,2]) * 0.99, max(networkLayout[,2]) * 1.01),
+      vertex.color=rainbow(max(membership(comms)+1))[membership(comms)+1],
+      vertex.size = 700 + (90 * degree(stringNetwork)),
+      vertex.label = NA,
+      main = "Network from STRING data"
+    )
+
+# With these settings, size of a node increases with degree, colour is dictated by 
+# community membership. We can see that there are several fairly well connected nodes 
+# with high degree of connectivity to other nodes. These nodes tend to be members of 
+# larger communities - this makes sense biologically, we could expect a protein in a 
+# large group of interacting proteins has many interactions. There are two distinctly 
+# large and well connected communities.
+
+
+
+
+
+
+
+
 
 #What is the size gap in our scCCnet based graph between the 1st and 2nd
 #largest communities?
 set.seed(1234)
-igraphToSize(myG)
+getSize(stringNetwork)
 
 #The size gap is 6. How does this compare to a randomly generated graph?
 
 #Produce 5 random graphs
 rGs <- list()
 for (i in seq(5)){
-  rGs[[i]] <- igraphToRIgraph(myG)
+  rGs[[i]] <- igraphToRIgraph(stringNetwork)
 }
 
 #Find the size gap for each graph
-rGSamples <- lapply(rGs, igraphToSize)
+rGSamples <- lapply(rGs, getSize)
 rGSamples <- unlist(rGSamples)
 
 #Create a histogram of size gaps in randomly generated graphs
@@ -103,7 +144,7 @@ axis(side = 1, at = min(rGSamples):max(rGSamples))
 #structure comes solely from the call to cluster_infomap(). However, the mean
 #will be meaningful to compare with the random graph samples
 
-myGSamples <- replicate(5, igraphToSize(myG))
+myGSamples <- replicate(5, getSize(stringNetwork))
 
 brk <- seq(min(myGSamples)-0.5, max(myGSamples)+0.5, by=1)
 hist(myGSamples, breaks = brk, col="cornflowerblue",
