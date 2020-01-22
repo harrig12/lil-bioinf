bf4342eb5095c6e86831a8fb1764af1dd2a2f51c
Cait <cait.harrigan@mail.utoronto.ca>
2020-01-21 20:06:14 -0500

    v1.1

--- plotting.R
+++ plotting.R
@@ -1,7 +1,7 @@
 # plotting.R
 # Author: Cait Harrigan
-# Date: November 2019
-# Version 1.0
+# Date: Jan 2020
+# Version 1.1
 
 # =============================================================================
 # 1. Set Up
@@ -21,50 +21,8 @@ load("~/Downloads/scCCnet.rdata")
 preparedData <- scCCnet
 #load("./STRING.RData")
 
-
 # =============================================================================
-# 2. Define functions
-# =============================================================================
-
-# We will define a few functions that let us do certain operations on the data.
-
-getGraphSize <- function(G){
-  # Find the difference in sizes between the largest 
-  # and second largest communities in graph G
-  # Parameters:
-  #   G: an igraph
-  #
-  # Value: an integer
-  
-  # get cluster sizes
-  sizes <- sizes(cluster_infomap(G))
-  
-  # order sizes as a vector
-  sizesOrdered <- as.vector(sizes)
-  
-  # in some cases there is only one community
-  if (length(sizesOrdered) == 1){
-    # in this case, only return this community
-    return(sizesOrdered[1])
-  }
-  
-  # find difference between first and second largest community
-  return(sizesOrdered[1] - sizesOrdered[2])
-}
-
-randGraph <- function(G){
-  # Make a random graph of same degree as an input igraph G
-  # Parameters:
-  #   G: an igraph
-  #
-  # Value: an undirected igraph with same degree as G
-  
-  d <- degree(G)
-  return(sample_degseq(d, method = "vl"))
-}
-
-# =============================================================================
-# 3. Plot network
+# 2. Plot network
 
 # This sections is modified from Boris Steipe's 
 # FND-MAT-Graphs_and_networks.R Version 1.0 
@@ -81,7 +39,12 @@ stringNetwork <- graph_from_edgelist(as.matrix(preparedData[c(1,2)]), directed =
 
 # With functions from iGraph package, prepare network for plotting
 communities <- cluster_infomap(stringNetwork)
+
+# Play with the parameters of this function for cool networks
 networkLayout <- layout_with_graphopt(stringNetwork, charge = 0.002, mass=100, spring.constant = 0.5)
+
+# Have a look at other possible layout algorithms available
+help.search("layout_with")
 
 # Plot!
 plot( stringNetwork,
